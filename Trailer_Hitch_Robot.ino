54ea015e1f23815f1cb17f1c105a394599ae80ac
sparrot <stephanie@integrated.net>
2016-09-04 17:43:18 -0400

    Update Trailer_Hitch_Robot.ino
    put Blen's and Laura's code up. Does not compile, Laura's needs work. After trying on Arduino (all code except Laura's), the lights do not work the way this code is, needs to be fixed

--- Trailer_Hitch_Robot.ino
+++ Trailer_Hitch_Robot.ino
@@ -1,4 +1,6 @@
 #include <XBOXRECV.h>
+#include "pitches.h"                    //need to make this header file
+
 
 const int actrCtrlPin = 2;              //Pin to control actuator movement
 const int hallSensorPin = A0;           //Pin to read Hall Effect Sensor
@@ -10,6 +12,8 @@ const int actrDown = 100;               //Value used to retract actuator
 const int actrHold = 0;                 //Value used to hold actuator in position
 int sensorOK = 0;
 
+int PIN_BUTTON_A = 20;
+int PIN_BUTTON_B = 21;
 
 int motorDir = 8; //Dir 1
 int motorSpeed = 9; //PWM 1
@@ -18,6 +22,23 @@ int motorSpeed2 = 11; //PWM2
 
 int ultraSensorOK = 12;
 int buzzardBackwards = 13;               //backwards buzzard
+
+
+//these outputs are all for testing purposes 
+int forwardLED = 22; 
+int leftLED = 23;
+int rightLED = 24;
+int upLED = 25; 
+int downLED = 26; 
+int stopLED = 27;
+
+int melody[] = {                         //select the notes you want to play
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4 
+};
+
+int noteDurations[] = {                 //4= quarter note, 8 = either note
+   4, 8, 8, 4, 4, 4, 4, 4
+};
 
 USB Usb;
 XBOXRECV Xbox(&Usb);
@@ -32,6 +53,17 @@ void setup() {
   pinMode(motorSpeed2, OUTPUT); //Movement Part
   pinMode(ultraSensorOK, INPUT); //sensor okay to move?
   pinMode(buzzardBackwards, INPUT); //beeping for backwards
+//pinMode(buzzer, OUTPUT); //buzzer
+  pinMode(forwardLED, OUTPUT);
+  pinMode(leftLED, OUTPUT);
+  pinMode(rightLED, OUTPUT);
+  pinMode(upLED, OUTPUT);
+  pinMode(downLED, OUTPUT);
+  pinMode(stopLED, OUTPUT);
+  pinMode(PIN_BUTTON_A, INPUT);
+  digitalWrite(PIN_BUTTON_A, LOW);  //question
+  pinMode(PIN_BUTTON_B, INPUT);
+  digitalWrite(PIN_BUTTON_B, LOW);  //question
 }
 
 void loop() {
@@ -68,6 +100,14 @@ void loop() {
     else if (Xbox.getAnalogHat(LeftHatX, 0) < 1000) {   
       //going left???
     }
+    else if (digitalRead(PIN_BUTTON_A) == LOW) {
+    // Button is pressed
+    ButtonA_pressed();
+    }
+    else if (digitalRead(PIN_BUTTON_B) == LOW) {
+    // Button is pressed
+    ButtonB_pressed();
+    }
     else {                                              //If Right Stick is not moved
       actuatorHold();
     }
@@ -78,10 +118,12 @@ delay(1);
 
 void actuatorUp() {
   analogWrite(actrCtrlPin, actrUp);
+  digitalWrite(upLED, HIGH);
 }
 
 void actuatorDown() {
   analogWrite(actrCtrlPin, actrDown);
+  digitalWrite(downLED, HIGH);
 }
 
 void actuatorHold() {
@@ -96,7 +138,7 @@ void ForwardMovement() {
     sensorOK = 1;
     if (ultraSensorOK >= 10) {
       //continue full speed ahead!
-      
+      digitalWrite(forwardLED, HIGH);
     }
     else if (ultraSensorOK >= 8) {
       //continue a third of the speed
@@ -130,5 +172,36 @@ void BackwardMovement() {
   
     analogWrite(motorSpeed, 1); //motors same speed but slow
     analogWrite(motorSpeed2, 1); //motors same speed but slow
-  
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    //use this to calculate noteDuration: quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(13);
+  }
+}
+
+//Laura
+
+void ButtonA_pressed(){
+if (PIN_BUTTON_A) == LOW) {                        // Button A is pressed
+  //both motors move forward at same speed at certain time();
+}
+  else {
+    //both motors move backward at same speed at certain time();
+  }
+}
+
+void ButtonB_pressed(){
+if (PIN_BUTTON_B) == LOW) {                     // Buttom B is pressed
+  //all motors stop ();
+}
+  else {
+    //press BUTTON_B to start the system ();
+  }
 }
