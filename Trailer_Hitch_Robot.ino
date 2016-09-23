bfe2a7551286917bf8bc2891b948f0ab0a5f8db0
rklavin <rklav12@hotmail.com>
2016-09-22 20:58:20 -0400

    Update Trailer_Hitch_Robot.ino
    Removed all ultrasonic sensor components, buzzer components, and debug components.

--- Trailer_Hitch_Robot.ino
+++ Trailer_Hitch_Robot.ino
@@ -1,6 +1,5 @@
 #include <XBOXRECV.h>
 #include <Servo.h>
-#include "pitches.h"
 
 //need a pin for emergency stop button(s)
 
@@ -15,7 +14,6 @@ const int limitBot = 0;                 //Value from Hall Effect Sensor for max 
 const int actrUp = 100;                 //Value used to extend actuator
 const int actrDown = 100;               //Value used to retract actuator
 //const int actrHold = 0;                 //Value used to hold actuator in position
-bool sensorOK = true;                   //Boolean value checked when left stick is moved
 bool emergencyStop = false;             //Boolean value, prevents all inputs while true, must reset arduino to continue use
 
 //Motor variables
@@ -31,31 +29,9 @@ const int stationary = 92;              //Value to send to motor controller for 
 const int forwardMin = 94;              //Value to send to motor controller for minimum forward speed
 const int forwardMax = 141;             //Value to send to motor controller for maximum forward speed
 
-//Ultrasonic sensor variables
-const int ultraSensorTX = 14;           //Pin for trig
-const int ultraSensorRX = 15;           //Pin for echo
-const int buzzardBackwards = 13;        //Pin to send signal to buzzer
-
 //Controller variables
 const int conThresh = 7500;             //Value to check joystick position against for activation (minimum press to move)
 const int conLimit = 32767;             //Value for the limit of joystick position (needs verified)
-
-//These outputs are all for testing purposes 
-int forwardLED = 22; 
-int leftLED = 23;
-int rightLED = 24;
-int upLED = 25; 
-int downLED = 26; 
-int stopLED = 27;
-int backwardLED = 30;
-
-int melody[] = {                         //select the notes you want to play
-  NOTE_E2/*, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4 */
-};
-
-int noteDurations[] = {                 //4= quarter note, 8 = either note
-   1/*, 4, 4, 4 , 4, 4, 4, 4*/
-};
 
 USB Usb;
 XBOXRECV Xbox(&Usb);
@@ -66,18 +42,6 @@ void setup() {
   actuator.attach(actrCtrlPin);
   motor1.attach(motorSpeed1Pin);
   motor2.attach(motorSpeed2Pin);
-  //pinMode(buzzardBackwards, INPUT); //beeping for backwards
-  pinMode(ultraSensorRX, INPUT);
-  pinMode(ultraSensorTX, OUTPUT);
-  
-  //Debug pins
-  pinMode(forwardLED, OUTPUT);
-  pinMode(leftLED, OUTPUT);
-  pinMode(rightLED, OUTPUT);
-  pinMode(upLED, OUTPUT);
-  pinMode(downLED, OUTPUT);
-  pinMode(stopLED, OUTPUT);
-  pinMode(backwardLED, OUTPUT);
 }
 
 void loop() {
@@ -85,51 +49,36 @@ void loop() {
     Usb.Task();
     if (Xbox.XboxReceiverConnected) {
       bool moving = false;
-      if (sensorOK) {
-        if (Xbox.getAnalogHat(LeftHatY, 0) > conThresh) {
-          //going forward
-          if (Xbox.getButtonClick(Y, 0)) {
-            ForwardMovement(forwardMax);
-          } else {
-            ForwardMovement(forwardMax/4);
-          }
-          moving = true;
-        } 
-        else if (Xbox.getAnalogHat(LeftHatY, 0) < -conThresh) {   
-          //going backwards
-          //buzzard
-          if (Xbox.getButtonClick(Y, 0)) {
-            BackwardMovement(reverseMax);
-          } else {
-            BackwardMovement(reverseMax/4);
-          }
-          moving = true;
+      if (Xbox.getAnalogHat(LeftHatY, 0) > conThresh) {
+        //going forward
+        if (Xbox.getButtonClick(Y, 0)) {
+          ForwardMovement(forwardMax);
+        } else {
+          ForwardMovement(forwardMax/4);
         }
-        else if (Xbox.getAnalogHat(LeftHatX, 0) > conThresh) {
-          //going right???
-          digitalWrite(rightLED, HIGH);
-          moving = true;
-        } 
-        else if (Xbox.getAnalogHat(LeftHatX, 0) < -conThresh) {   
-          //going left???
-          digitalWrite(leftLED, HIGH);
-          moving = true;
+        moving = true;
+      } 
+      else if (Xbox.getAnalogHat(LeftHatY, 0) < -conThresh) {   
+        //going backwards
+        //buzzard
+        if (Xbox.getButtonClick(Y, 0)) {
+          BackwardMovement(reverseMax);
+        } else {
+          BackwardMovement(reverseMax/4);
         }
-        else {
-          NoMovement();
-          digitalWrite(forwardLED, LOW);
-          digitalWrite(leftLED, LOW);
-          digitalWrite(rightLED, LOW);
-          digitalWrite(backwardLED, LOW);
-          moving = false;
-        }
+        moving = true;
+      }
+      else if (Xbox.getAnalogHat(LeftHatX, 0) > conThresh) {
+        //going right???
+        moving = true;
+      } 
+      else if (Xbox.getAnalogHat(LeftHatX, 0) < -conThresh) {   
+        //going left???
+        moving = true;
       }
       else {
         NoMovement();
-        if (Xbox.getButtonClick(A, 0)) {
-          // Button is pressed
-         ButtonA_pressed();
-        }
+        moving = false;
       }
 
       if (!moving) {
@@ -138,34 +87,24 @@ void loop() {
         if (Xbox.getAnalogHat(RightHatY , 0) > conThresh) {      //Check if Right Stick is in up direction
           if (pos < limitTop) {                               //Check if actuator can extend
             actuatorUp();
-            digitalWrite(upLED, HIGH);
           } else {
             actuatorHold();
-            digitalWrite(upLED, LOW);
-            digitalWrite(downLED, LOW);
           }
         } 
         else if (Xbox.getAnalogHat(RightHatY, 0) < -conThresh) { //Check if Right Stick is in down direction
           if (pos > limitBot) {                               //Check if actuator can retract
             actuatorDown();
-            digitalWrite(downLED, HIGH);
           } 
           else {
             actuatorHold();
-            digitalWrite(upLED, LOW);
-            digitalWrite(downLED, LOW);
           }
         } 
         else {                                              //If Right Stick is not moved
           actuatorHold();
-          digitalWrite(upLED, LOW);
-          digitalWrite(downLED, LOW);
         }
       }
       else {
         actuatorHold();
-        digitalWrite(upLED, LOW);
-        digitalWrite(downLED, LOW);
       }
       
      if (Xbox.getButtonClick(B, 0)) {
@@ -181,7 +120,6 @@ void loop() {
   }
   else {
     NoMovement();
-    digitalWrite(stopLED, HIGH);
   }
 }
 
@@ -189,103 +127,33 @@ void actuatorUp() {
   int stick = Xbox.getAnalogHat(RightHatY, 0);
   int movement = map(stick, conThresh, conLimit, forwardMin, forwardMax);
   actuator.write(movement);          //or use actrUp to specify constant value
-  digitalWrite(upLED, HIGH);
 }
 
 void actuatorDown() {
   int stick = Xbox.getAnalogHat(RightHatY, 0);
   int movement = map(stick, -conThresh, -conLimit, reverseMin, reverseMax);
   actuator.write(movement);          //or use actrDown to specify constant value
-  digitalWrite(downLED, HIGH);
 }
 
 void actuatorHold() {
   actuator.write(stationary);
-  digitalWrite(upLED, LOW);
-  digitalWrite(downLED, LOW);
 }
 
 
 
 //Stephanie Stuff for movement
 void ForwardMovement(int maxForward) {
-  long duration, distance;
   int stick = Xbox.getAnalogHat(LeftHatY, 0);
-  
-  //getting sensor to transmit??
-  //will this give a delay?? We would need to make sure it is quick enough not to notice...
-  digitalWrite(ultraSensorTX, LOW);
-  delayMicroseconds(10);
-  digitalWrite(ultraSensorTX, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(ultraSensorTX, LOW);
-  
-  duration = pulseIn(ultraSensorRX, HIGH);
-  distance = (duration/2) / 29.1;     //distance is in cm and is what value was returned
-  
-  if (distance > 25) {                //should give about 44 inches assuming 11 inch is 25
-    sensorOK = true;
-    if (distance >= 100) {
-      //continue full speed ahead!
-      int movement = map(stick, conThresh, conLimit, forwardMin, maxForward);
-      motor1.write(movement);
-      motor2.write(movement);
-      digitalWrite(forwardLED, HIGH);
-    }
-    else if (distance >= 75) {
-      //continue at 2 thirds of full speed
-      int max = forwardMin + (((maxForward - forwardMin)*2) / 3);
-      int movement = map(stick, conThresh, conLimit, forwardMin, maxForward);
-      motor1.write(movement);
-      motor2.write(movement);
-    }
-    else if (distance >= 50) {     
-      //continue at a third of full speed
-      int max = forwardMin + ((maxForward - forwardMin) / 3);
-      int movement = map(stick, conThresh, conLimit, forwardMin, maxForward);
-      motor1.write(movement);
-      motor2.write(movement);
-    }
-    else { //25 should be about 11 inches away from it
-      //continue a crawl of full speed
-      int max = forwardMin + ((maxForward - forwardMin) / 6);
-      int movement = map(stick, conThresh, conLimit, forwardMin, maxForward);
-      motor1.write(movement);
-      motor2.write(movement);
-    }
-  }
-  else {
-    //Robot is too close, wait for confirmation to continue moving
-    //Should give user feedback, check if blink middle LEDs is possible
-    sensorOK = false;
-    motor1.write(stationary);
-    motor2.write(stationary);
-  }
+  int movement = map(stick, conThresh, conLimit, forwardMin, maxForward);
+  motor1.write(movement);
+  motor2.write(movement);
 }
 
 void BackwardMovement(int maxReverse) {
-  //sensorOK = 1;
-  //buzzardBackwards = 1; //figure out how to make it speak
-  
   int stick = Xbox.getAnalogHat(LeftHatY, 0);
   int movement = map(stick, -conThresh, -conLimit, reverseMin, maxReverse);
   motor1.write(movement);
   motor2.write(movement);
-  digitalWrite(backwardLED, HIGH);
-  
-  //for (int thisNote = 0; thisNote < 3; thisNote++) {
-    //use this to calculate noteDuration: quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int thisNote = 0;
-    int noteDuration = 10000 / noteDurations[thisNote];
-    tone(buzzardBackwards, melody[thisNote], noteDuration);
-    //to distinguish the notes, set a minimum time between them.
-    //the note's duration + 30% seems to work well:
-    //int pauseBetweenNotes = noteDuration * 1.30;
-    //delay(pauseBetweenNotes);
-    //stop the tone playing:
-    //tone(buzzardBackwards, melody[thisNote], noteDuration);
-  noTone(13);
-  //} 
 }
 
 void NoMovement() {
